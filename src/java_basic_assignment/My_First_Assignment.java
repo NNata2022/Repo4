b53fd04d857af8839da0f2125fede65fdd673a79
NNata2022 <nagulanata@gmail.com>
2022-10-08 23:13:28 -0500

    first commit

---
+++ src/java_basic_assignment/My_First_Assignment.java
@@ -0,0 +1,17 @@
+package java_basic_assignment;
+
+public class My_First_Assignment {
+
+	public static void main(String[] args) {
+		System.out.println(Hello, its my first assignment);
+		System.out.println(Good day, its my first assignment);
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
